b8c34449a897713a3726d9e71770cfbc3941bfa8
Zifau <33486013+zifaucode@users.noreply.github.com>
2021-11-05 13:05:46 +0700

    fizzbuzz

--- coding-test.php
+++ coding-test.php
@@ -34,3 +34,27 @@ $hasil = str_replace("Banget", "sekali", $expInput[3]);
 echo $hasil;
 ?>
 
+
+
+//Fizz Buzz dengan PHP
+<?php
+for($i=1;$i<100;$i++){
+
+    if ($i%3==0 && $i%5==0){
+    echo 'FizzBuzz';
+    }
+	else if($i%3==0){
+    echo 'Fizz';
+    }
+    else if($i%5==0){
+    echo 'Buzz';
+    }
+    else {
+    echo $i;
+    }
+    
+    echo"<br>";
+    
+}
+?> 
+
